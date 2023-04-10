e6aee72eeb8deb8e1c243acd119ff6bf7eab45a2
Oliver Karstoft <orkarstoft@gmail.com>
2023-04-10 21:55:10 +0200

    bug: Log presented wrong unit (#18)
    * The functionality is good but the log sucks
    
    * Missing new line

--- pkg/pubsub/Listen.go
+++ pkg/pubsub/Listen.go
@@ -29,7 +29,7 @@ func Listen() error {
 	// Receive messages
 	err = subscription.Receive(ctx, func(ctx context.Context, msg *pubsub.Message) {
 		if config.Config.Debug {
-			fmt.Printf("[DEBUG]: Received message: %s", string(msg.Data))
+			fmt.Printf("[DEBUG]: Received message: %s\n", string(msg.Data))
 		}
 
 		// Unmarshal message
@@ -41,14 +41,14 @@ func Listen() error {
 		if m.Action == "kscale_scale_namespace_up" {
 			fmt.Printf("[INFO]: Scaling %s namespace %s up\n", m.Cluster, m.Namespace)
 			convertIntToTimeDuration, err := time.ParseDuration(fmt.Sprintf("%dh", m.Duration))
+			if err != nil {
+				panic(err)
+			}
 
 			if config.Config.Debug {
 				fmt.Printf("[DEBUG]: Duration: %d, Duration in time.Duration: %s\n", m.Duration, convertIntToTimeDuration)
 			}
 
-			if err != nil {
-				panic(err)
-			}
 			k8s.ScaleNamespaceUp(m.Namespace, convertIntToTimeDuration)
 		}
 
