ef68df9c50c82b9a616009e4064de26f15a9a559
Oliver Karstoft <orkarstoft@gmail.com>
2023-05-05 09:56:35 +0200

    Expanding config

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -4,11 +4,12 @@ import (
 	"log"
 
 	"github.com/kelseyhightower/envconfig"
+	"github.com/orkarstoft/kscale/pkg/instancemetadata"
 )
 
 type config struct {
 	ProjectID   string `envconfig:"PROJECT_ID" required:"true"`
-	ClusterName string `envconfig:"CLUSTERNAME" required:"true"`
+	ClusterName string `envconfig:"CLUSTERNAME" required:"false"`
 	Topic       string `envconfig:"TOPIC" required:"true"`
 	Debug       bool   `envconfig:"DEBUG" default:"false"`
 }
@@ -19,5 +20,17 @@ func Init() {
 	err := envconfig.Process("KSCALE", &Config)
 	if err != nil {
 		log.Fatal(err.Error())
+	}
+
+	if Config.ProjectID == "" || Config.Topic == "" {
+		log.Fatal("Missing required env variables")
+	}
+
+	if Config.ClusterName == "" {
+		clustername, err := instancemetadata.GetGCPInstanceMetadata()
+		if err != nil {
+			log.Fatalf("Failed to get cluster name from GCP Instance Metadata: %v", err)
+		}
+		Config.ClusterName = clustername
 	}
 }
