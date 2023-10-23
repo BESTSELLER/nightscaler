89a99d72d32f19380744c07230e18bc19b3fc096
Lasse Gaardsholt <lasse.gaardsholt@bestseller.com>
2023-10-23 21:07:45 +0200

    Too tired to write descriptive message
    Signed-off-by: Lasse Gaardsholt <lasse.gaardsholt@bestseller.com>

--- main.go
+++ main.go
@@ -4,7 +4,6 @@ import (
 	"time"
 
 	"github.com/BESTSELLER/nightscaler/config"
-	"github.com/BESTSELLER/nightscaler/k8s"
 	"github.com/BESTSELLER/nightscaler/logger"
 	"github.com/BESTSELLER/nightscaler/pubsub"
 )
@@ -16,30 +15,16 @@ func main() {
 	logger.Log.Info().Msg("Starting nightscaler")
 	logger.Log.Info().Msgf("Creating subscriber for topic %s in project %s", config.Config.ListenTopic, config.Config.ProjectID)
 
-	go sendNamespaces()
+	go func() {
+		for {
+			pubsub.SendNamespaces()
+			time.Sleep(10 * time.Minute)
+		}
+	}()
 
 	err := pubsub.Listen()
 	if err != nil {
 		logger.Log.Fatal().Err(err).Msg("failed to listen for Pub/Sub messages")
 	}
 
-}
-
-func sendNamespaces() {
-	for {
-		namespaces, err := k8s.List()
-		if err != nil {
-			logger.Log.Fatal().Err(err).Msg("failed to list namespaces")
-		}
-
-		err = pubsub.Publish(namespaces, pubsub.Attributes{
-			Entity: "namespace",
-			Action: "create",
-		})
-		if err != nil {
-			logger.Log.Fatal().Err(err).Msg("failed to send Pub/Sub message")
-		}
-
-		time.Sleep(10 * time.Minute)
-	}
 }
